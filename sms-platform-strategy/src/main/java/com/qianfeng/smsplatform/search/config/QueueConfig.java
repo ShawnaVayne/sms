62d174b9e5617f1c3504212f7a5d5ffb3cb59911
xsh <475356349@qq.com>
2019-12-03 20:32:14 +0800

    更新了QueueConfig

--- sms-platform-strategy/src/main/java/com/qianfeng/smsplatform/search/config/QueueConfig.java
+++ sms-platform-strategy/src/main/java/com/qianfeng/smsplatform/search/config/QueueConfig.java
@@ -1,9 +1,13 @@
 package com.qianfeng.smsplatform.search.config;
 
 
+import com.qianfeng.smsplatform.common.constants.RabbitMqConsants;
 import com.qianfeng.smsplatform.search.util.ConnectionUtils;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 
@@ -13,12 +17,25 @@ import java.util.concurrent.TimeoutException;
 /**
  * @Author 徐胜涵
  */
-@Component
+@Configuration
 public class QueueConfig {
 
-    public void declareQueue(String queueName) throws IOException, TimeoutException {
-        Connection connection = ConnectionUtils.getConnection();
-        Channel channel = connection.createChannel();
-        channel.queueDeclare(queueName, true, false, false, null);
+    @Bean
+    public Queue gateWayQueue() {
+        return new Queue(RabbitMqConsants.TOPIC_SMS_GATEWAY);
     }
+    //下发日志
+    @Bean
+    public Queue logQueue() {
+        return new Queue(RabbitMqConsants.TOPIC_SMS_SEND_LOG);
+    }
+    //状态报告
+    public @Bean Queue reportQueue() {
+        return new Queue(RabbitMqConsants.TOPIC_PUSH_SMS_REPORT);
+    }
+    //更新状态报告
+    public @Bean Queue updateQueue() {
+        return new Queue(RabbitMqConsants.TOPIC_UPDATE_SMS_REPORT);
+    }
+
 }
