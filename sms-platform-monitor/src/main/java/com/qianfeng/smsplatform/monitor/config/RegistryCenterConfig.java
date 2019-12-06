1f0b51c24ab4dc2d66b01c60b4387d653ad3a2dc
sjw <1041581367@qq.com>
2019-12-06 10:05:53 +0800

    缓存更新完成

---
+++ sms-platform-monitor/src/main/java/com/qianfeng/smsplatform/monitor/config/RegistryCenterConfig.java
@@ -0,0 +1,25 @@
+package com.qianfeng.smsplatform.monitor.config;
+
+import com.dangdang.ddframe.job.reg.zookeeper.ZookeeperConfiguration;
+import com.dangdang.ddframe.job.reg.zookeeper.ZookeeperRegistryCenter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Classname RegistryCenterConfig
+ * @Description TODO
+ * @Date 2019/12/5 22:07
+ * @Created by sunjiangwei
+ */
+@Configuration
+@ConditionalOnExpression("'${regCenter.serverList}'.length()>0")
+public class RegistryCenterConfig {
+
+    @Bean(initMethod = "init")
+    public ZookeeperRegistryCenter regCenter(@Value("${regCenter.serverList}") final String serverList,
+                                             @Value("${regCenter.namespace}") final String namespace){
+        return new ZookeeperRegistryCenter(new ZookeeperConfiguration(serverList,namespace));
+    }
+}
